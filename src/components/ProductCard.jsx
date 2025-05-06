0ae482d010df4714c0306f720b2f5c165d4d1aca
Dana <danalkwifi@gmail.com>
2025-05-06 16:58:31 -0400

    In progress

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -5,34 +5,31 @@ import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons/faSquareCheck';
 
 export default function ProductCard({search}) {
-    const [edit, setEdit] = useState(false)
+    const [edit, setEdit] = useState(null) //Got help from ChatGPT
     const [update, setUpdate] = useState("")
     //onClick
     //change h3 into input 
     //how to save the update the js file?
 
     const handleEdit = (index) => {
-        if(edit){
-            setEdit(false)
-        }else{
-            setEdit(true)
-            setUpdate(data[index].name)
-            console.log(update)
-            
-        }
-        //console.log(edit)
+        setEdit(index)
+        setUpdate(data[index].name)
+        console.log(update)     
     }
+
 
     const handleUpdateName = (index) => {
-        s
-        //console.log(data[index].name)
+        // saveing the object - updateData =  ...data
+        // updateData - data[index].name = update
+        //set the update back to null
+        setEdit(false)
+        const newData = {...data};
+        newData[index].name = update
+    
+        console.log(newData[index].name)
+        
     }
     
-    const handleDescriptionUpdate = (index) => {
-        console.log(data[index].name)
-    }
-
-    //console.log(data[0].name)
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-8 p-8 mx-8 md:mx-12 ">
        
@@ -41,7 +38,7 @@ export default function ProductCard({search}) {
                 return (
                     <div key={item.id} className='border-3 rounded-xl border-blue-300 m-3 p-3 hover:border-blue-500' > 
                         <img className='w-full object-cover h-48 border-0 rounded-xl' src={item.image} alt={item.name} />
-                            {edit ? (
+                            {edit === index ? (
                                 <div className='flex flex-row justify-center items-center p-2'>
                                     <input className="border-2 rounded-xl px-4 py-2 mt-2" 
                                         type="text"
@@ -51,7 +48,7 @@ export default function ProductCard({search}) {
                                     
                                     
                                     />
-                                    <button className='hover:cursor-pointer mx-3 text-2xl' onClick={() => handleUpdateName(item)}><FontAwesomeIcon icon={faSquareCheck} /></button>
+                                    <button className='hover:cursor-pointer mx-3 text-2xl' onClick={() => handleUpdateName(index)}><FontAwesomeIcon icon={faSquareCheck} /></button>
 
                                 </div>
                             )
@@ -67,7 +64,7 @@ export default function ProductCard({search}) {
                         
                         <div className='flex flex-row justify-between items-center p-2'>
                             <p className='text-sm px-2'>{item.description}</p>
-                            <button className='hover:cursor-pointer px-3' onClick={() => handleDescriptionUpdate(index)}><FontAwesomeIcon icon={faPenToSquare} /></button>
+                            <button className='hover:cursor-pointer px-3'><FontAwesomeIcon icon={faPenToSquare} /></button>
                         </div>
                     </div>
                 )
