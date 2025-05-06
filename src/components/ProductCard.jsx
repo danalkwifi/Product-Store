831671a5c0c9b16f769142e1afbd62e3bc8f30d2
Dana <danalkwifi@gmail.com>
2025-05-06 13:34:11 -0400

    Edit Feature in progress

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,20 +1,68 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { data } from '../product.js'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPenToSquare } from '@fortawesome/free-solid-svg-icons';
+import { faSquareCheck } from '@fortawesome/free-solid-svg-icons/faSquareCheck';
 
 export default function ProductCard({search}) {
-    
+    const [edit, setEdit] = useState(false)
+    const [update, setUpdate] = useState("")
+    //onClick
+    //change h3 into input 
+    //how to save the update the js file?
 
-    //console.log(search)
+    const handleEdit = () => {
+        setEdit(true)
+        
+    }
+
+    const handleUpdateName = (index) => {
+        setEdit(false)
+        setUpdate(data[index].name)
+        console.log(data[index].name)
+    }
+    
+    const handleDescriptionUpdate = (index) => {
+        console.log(data[index].name)
+    }
+
+    //console.log(data[0].name)
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-8 p-8 mx-8 md:mx-12">
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-3 gap-y-8 p-8 mx-8 md:mx-12 ">
        
-        {data.filter(item => item.name.toLowerCase().includes(search.toLowerCase().trim()))
-            .map((item) => {
+        {data.filter(item => item.name.toLowerCase().includes(search.toLowerCase().trim()) | item.description.toLowerCase().includes(search.toLowerCase().trim()))
+            .map((item, index) => {
                 return (
-                    <div key={item.id} className='border-3 rounded-xl border-blue-300 m-3 p-3 hover:border-blue-500 hover:border-4 hover:cursor-pointer' > 
+                    <div key={item.id} className='border-3 rounded-xl border-blue-300 m-3 p-3 hover:border-blue-500' > 
                         <img className='w-full object-cover h-48 border-0 rounded-xl' src={item.image} alt={item.name} />
-                        <h3 className='text-md md:text-lg font-semibold text-center pt-2'>{item.name}</h3>
-                        <p className='text-sm mt-3'>{item.description}</p>
+                            {edit ? (
+                                <div className='flex flex-row justify-center items-center p-2'>
+                                    <input className="border-2 rounded-xl px-4 py-2 mt-2" 
+                                        type="text"
+                                        placeholder={item.name} 
+                                        value={update}
+                                        onChange={(e) => setUpdate(e.target.value)}
+                                    
+                                    
+                                    />
+                                    <button className='hover:cursor-pointer mx-3 text-2xl' onClick={() => handleUpdateName(item)}><FontAwesomeIcon icon={faSquareCheck} /></button>
+
+                                </div>
+                            )
+                            :
+                            ( 
+                                <div className='flex flex-row justify-center items-center p-2'>
+                                    <h3 className='text-md md:text-lg font-semibold'>{item.name}</h3>
+                                    <button className='hover:cursor-pointer px-3' onClick={() => handleEdit()}><FontAwesomeIcon icon={faPenToSquare} /></button>
+                                </div>
+                            )
+                            }
+                            
+                        
+                        <div className='flex flex-row justify-between items-center p-2'>
+                            <p className='text-sm px-2'>{item.description}</p>
+                            <button className='hover:cursor-pointer px-3' onClick={() => handleDescriptionUpdate(index)}><FontAwesomeIcon icon={faPenToSquare} /></button>
+                        </div>
                     </div>
                 )
             })
